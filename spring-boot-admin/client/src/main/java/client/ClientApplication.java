65e3230d69864f010644bb48534d24aa67b67fc8
steelxiang <steelxiang@live.com>
2019-08-18 17:29:39 +0800

    Initial commit

---
+++ spring-boot-admin/client/src/main/java/client/ClientApplication.java
@@ -0,0 +1,17 @@
+package client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiang
+ * @date 2019/8/18
+ */
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+
+    }
+}
